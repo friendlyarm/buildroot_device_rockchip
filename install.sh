d54c3221d5b4533bfd2a2dd2f63bce5ea6266db4
Lawrence-Tang <tangzongsheng@gmail.com>
2024-09-28 14:37:42 +0800

    Merge from upstream tag 'linux-5.10-gen-rkr8'

--- install.sh
+++ install.sh
@@ -7,8 +7,13 @@ pushd () {
 popd () {
     command popd "$@" > /dev/null
 }
+log_info() {
+    local now=`date +%s`
+    printf "\033[1;32m[INFO]: $* \033[0m \n"
+}
 pushd ../../
 export SDK_DIR=$PWD
+export SCRIPTS_DIR=$PWD/device/rockchip/common/scripts
 export RK_TOOL_DIR=$PWD/device/rockchip/common/tools
 export RK_SESSION=1
 export FRIENDLYELEC_SESSION=1
@@ -25,27 +30,43 @@ popd
 # apply rockchip post-hooks scripts
 ./common/fonts/install.sh $ROOTFS_DIR
 ./common/tools/install.sh $ROOTFS_DIR
+
+## {{
 pushd common/post-hooks
-./05-udev.sh $ROOTFS_DIR
-./06-async-commit.sh $ROOTFS_DIR
-./10-os-release.sh $ROOTFS_DIR
-sed -i "s/^BUILD_INFO=.*/BUILD_INFO=\"bot@friendlyelec $(date +%Y-%m-%d)\"/" $ROOTFS_DIR/etc/os-release
-./30-fstab.sh $ROOTFS_DIR && sed -i '/\/dev\/root/d' $ROOTFS_DIR/etc/fstab
-./40-busybox-reboot.sh $ROOTFS_DIR
-./50-locale.sh $ROOTFS_DIR
-./60-dirs.sh $ROOTFS_DIR
-./90-overlay.sh $ROOTFS_DIR
-./99-owner.sh $ROOTFS_DIR
+declare -a hookscripts=()
+#disable# hookscripts+=("00-wifibt.sh")
+#disable# hookscripts+=("01-hostname.sh")
+hookscripts+=("02-usb.sh")
+hookscripts+=("05-udev.sh")
+hookscripts+=("06-async-commit.sh")
+#disable# hookscripts+=("09-disk.sh")
+hookscripts+=("10-os-release.sh")
+#disable# hookscripts+=("20-info.sh")
+hookscripts+=("30-fstab.sh")
+hookscripts+=("40-busybox-reboot.sh")
+hookscripts+=("50-locale.sh")
+hookscripts+=("60-dirs.sh")
+hookscripts+=("90-overlay.sh")
+#disable# hookscripts+=("95-partitions.sh")
+hookscripts+=("99-owner.sh")
+for (( i=0; i<${#hookscripts[@]}; i++ ));
+do
+    ./${hookscripts[$i]} $ROOTFS_DIR
+    log_info "apply device/rockchip/common/post-hooks/${hookscripts[$i]} ret $?"
+done
 popd
+## }}
+sed -i "s/^BUILD_INFO=.*/BUILD_INFO=\"bot@friendlyelec $(date +%Y-%m-%d)\"/" $ROOTFS_DIR/etc/os-release
+sed -i '/\/dev\/root/d' $ROOTFS_DIR/etc/fstab
 
 ####
 # oem
 pushd ${SDK_DIR}
 [ -d ${ROOTFS_DIR}/oem ] || mkdir ${ROOTFS_DIR}/oem
-cp -af ${SDK_DIR}/device/rockchip/common/images/oem/oem_normal/* ${ROOTFS_DIR}/oem
+cp -af ${SDK_DIR}/device/rockchip/common/images/oem/normal/* ${ROOTFS_DIR}/oem
 # userdata
 [ -d ${ROOTFS_DIR}/userdata ] || mkdir ${ROOTFS_DIR}/userdata
-cp -af ${SDK_DIR}/device/rockchip/common/images/userdata/userdata_normal/* ${ROOTFS_DIR}/userdata
+cp -af ${SDK_DIR}/device/rockchip/common/images/userdata/normal/* ${ROOTFS_DIR}/userdata
 popd
 
 # clean
