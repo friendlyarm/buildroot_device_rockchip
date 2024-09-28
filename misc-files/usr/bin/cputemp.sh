bf798042c3a6a82956dbed624e2f5296ce3c12df
Lawrence-Tang <tangzongsheng@gmail.com>
2024-09-28 14:16:52 +0800

    update for tag rkr8: remove firmware, add ntpd, update usbdeivce script

--- misc-files/usr/bin/cputemp.sh
+++ misc-files/usr/bin/cputemp.sh
@@ -2,13 +2,16 @@
 
 while true
 do
-echo "-----------------"
-echo "Curr freq: `cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq`,`cat /sys/devices/system/cpu/cpu4/cpufreq/cpuinfo_cur_freq`"
-echo "Temp: `cat /sys/class/thermal/thermal_zone0/temp`"
-if [ -d /sys/devices/platform/pwm-fan ]; then
-    CUR=`cat /sys/devices/virtual/thermal/cooling_device0/cur_state`
-    MAX=`cat /sys/devices/virtual/thermal/cooling_device0/max_state`
-    echo "Fan Level: ${CUR}/${MAX}"
-fi
-sleep 5
+    echo "-----------------"
+    for path in /sys/devices/system/cpu/cpu*/cpufreq; do
+        CPU="$(echo $path | awk -F'/' '{print $6}')"
+        printf "${CPU}: %s\n" "$(cat ${path}/cpuinfo_cur_freq)"
+    done
+    echo "Temp: `cat /sys/class/thermal/thermal_zone0/temp` °C"
+    if [ -d /sys/devices/platform/pwm-fan ]; then
+        CUR=`cat /sys/devices/virtual/thermal/cooling_device0/cur_state`
+        MAX=`cat /sys/devices/virtual/thermal/cooling_device0/max_state`
+        echo "Fan Level: ${CUR}/${MAX}"
+    fi
+    sleep 5
 done
--- rk3328/disable-gui/install.sh
+++ rk3328/disable-gui/install.sh
@@ -2,12 +2,12 @@
 set -eu
 ROOTFS_DIR=$1
 
-services+=("50launcher")
+services+=("49weston 99chromium-wayland.sh")
 
 (cd ${ROOTFS_DIR} && {
     cd etc/init.d
     for (( i=0; i<${#services[@]}; i++ ));
     do
-        [ -d S${services[$i]} ] && mv S${services[$i]} K${services[$i]}
+        [ -f S${services[$i]} ] && mv S${services[$i]} K${services[$i]}
     done
 })
